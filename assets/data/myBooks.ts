b28be7717cbc3de040a63a2cea996797bf0b2f32
KAHLOMEH <52609450+KAHLOMEH@users.noreply.github.com>
2019-07-26 22:00:53 -0400

    Add files via upload

---
+++ assets/data/myBooks.ts
@@ -0,0 +1,9 @@
+import {BookInfo} from '../../app/BookInfo';
+
+export const MYBOOKS : BookInfo [] =
+[
+    {sname: 'The secrets', sauthor: 'Lisa Jwell', sgenre: 'Thriller', syear: '2018',spicture:'assets/images/books1.jpg'},
+    {sname: 'The Giver', sauthor: 'William K', sgenre: 'Love book', syear: '2016', spicture:'assets/images/book2.png'},
+    {sname: 'My life', sauthor: 'Keith Payne', sgenre: 'Mystery', syear: '2017', spicture:'assets/images/Book3.jpg'},
+    {sname: 'Milk & honey', sauthor: 'Dr. Joe Dispenza', sgenre: 'The science fiction', syear: '2013', spicture:'assets/images/th.jpg'}
+]
